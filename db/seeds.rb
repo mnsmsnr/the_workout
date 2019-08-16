a9468150b948ba23cfce6fd6ab6231ed8c064617
mns <vanillasky0127@yahoo.co.jp>
2019-08-17 02:46:41 +0900

    ホーム作成・materialize-sass導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# サンプルユーザー
+User.create!(name:  "Admin User",
+    email: "example@sample.co.jp",
+    password:              "password",
+    password_confirmation: "password",
+    admin:     true,
+    activated: true,
+    activated_at: Time.zone.now)
+  
+10.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@sample.co.jp"
+  password = "password"
+  User.create!(name:  name,
+      email: email,
+      password:              password,
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
+end
+  
+# サンプルメニュー
+10.times do |n|
+  training  = Faker::Name.training
+  content = "sample training menu#{n+1}"
+  Training.create!(training:  training,
+      content: content,
+      activated_at: Time.zone.now)
+end
+
+# サンプルコメント
+users = User.order(:created_at).take(6)
+10.times do
+    comment = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create!(comment: commnet) }
+end
