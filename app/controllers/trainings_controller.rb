033931407f9183f1ea8eb09ec889288453f01a43
mns <vanillasky0127@yahoo.co.jp>
2019-08-22 23:13:53 +0900

    トレーニング一覧・メニュー詳細ページ空ページ作成

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,6 @@
 class TrainingsController < ApplicationController
+    def index #トレーニング一覧・検索
+    end
+    def show #トレーニング詳細
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   get 'sighup' => 'users#new'
-
+  get 'training_index' => 'trainings#index'
+  get 'training' => 'trainings#show'
 
   root 'static_pages#home' #ルートパス
   resources :users
