d20b72c3d496a6ac630ecf911d93078b1b7ac4a8
mns <vanillasky0127@yahoo.co.jp>
2019-08-22 19:04:11 +0900

    プロフィールページ実装(フロント未完)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
     def new #サインアップ
         @user = User.new
     end
+
+    def show #プロフィール
+        @user = User.find(1) #find(params[:id])　仮実装
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'login' => 'sessions#new'
+  get 'sighup' => 'users#new'
+
 
   root 'static_pages#home' #ルートパス
   resources :users
