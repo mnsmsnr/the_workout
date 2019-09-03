3f6a71fcfb88f34582ff17eeb157529ccd388bc2
mns <vanillasky0127@yahoo.co.jp>
2019-09-03 19:00:26 +0900

    Training showレイアウト仮作成

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,5 +3,7 @@ class WorkoutsController < ApplicationController
         @workouts = Workout.paginate(page: params[:page])
     end
     def show #トレーニング詳細
+        @workout = Workout.find(1) #find(params[:id])　仮実装
+        @micropost = Micropost.find(1) #find(params[:id])　仮実装
     end
 end
