06606451b4af65f2f6831143262c2e5ff1d6559e
mns <vanillasky0127@yahoo.co.jp>
2019-09-03 23:14:27 +0900

    ページ間リンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home' #ホーム
   get 'login' => 'sessions#new' ##ログイン
   get 'sighup' => 'users#new' #新規登録
+  get 'profile' => 'users#show' #プロフィール
   get 'workout_index' => 'workouts#index' #ワークアウト検索
   get 'workout_menu' => 'workouts#show' #ワークアウト詳細
 
-  root 'static_pages#home' #ホーム
+  
   resources :workouts
   resources :users
   end
