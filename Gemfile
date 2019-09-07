dcb74bc8af98d265374d4b33cca8edcd463bd178
mns <vanillasky0127@yahoo.co.jp>
2019-09-07 13:15:32 +0900

    ユーザ新規登録機能実装

--- Gemfile
+++ Gemfile
@@ -20,7 +20,9 @@ gem 'faker',          '1.7.3'
 gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 
-# gem 'bcrypt',         '3.1.12'
+#has_secure_password関連
+gem 'bcrypt',         '3.1.12'
+
 # gem 'carrierwave',             '1.2.2'
 # gem 'mini_magick',             '4.7.0'
 # gem 'will_paginate',           '3.1.6'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
-    def new #サインアップ
-        @user = User.new
-    end
-
     def show #プロフィール
         @user = User.find(1) #find(params[:id])　仮実装
     end
+
+    def new #新規登録画面遷移
+        @user = User.new
+    end
+
+    def create #新規登録実行
+        @user = User.new(user_params)    # 実装は終わっていないことに注意!
+        if @user.save
+          # 保存の成功をここで扱う。
+        else
+          render 'new'
+        end
+    end
+
+    private
+
+        def user_params #新規登録用
+        params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+        end
 end
