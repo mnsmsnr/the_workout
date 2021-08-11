e331ab3b089a274b627bbc53608b77ea56aa5a6b
mnsmsnr <vanillasky0127@yahoo.co.jp>
2021-08-11 17:51:15 +0900

    テストworkデータ（仮）追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,8 +38,58 @@
 #ユーザ
 User.create!(name:  "TestUser",
     email: "example@sample.co.jp",
-    password:              "password",
+    password: "password2021",
 )
+
+#トレーニング
+Workout.create!(id: "1",
+    user_id: "0",
+    menu: "ベンチプレス",
+    content: "",
+    purpose: "1",
+    environment: "1",
+    target: "1",
+    video_id: "hB-kaLANF2A",
+)
+work = Workout.find(1)
+work.image.attach(io: File.open('app/assets/images/bench-press.jpg'), filename: 'bench-press.jpg')
+
+Workout.create!(id: "2",
+    user_id: "0",
+    menu: "プッシュアップ",
+    content: "",
+    purpose: "1",
+    environment: "3",
+    target: "1",
+    video_id: "VzUaVBXGVeQ",
+)
+work = Workout.find(2)
+work.image.attach(io: File.open('app/assets/images/push-up.jpg'), filename: 'push-up.jpg')
+
+Workout.create!(id: "3",
+    user_id: "0",
+    menu: "チンニング",
+    content: "",
+    purpose: "1",
+    environment: "3",
+    target: "2",
+    video_id: "hj2j-RsUiSE",
+)
+work = Workout.find(3)
+work.image.attach(io: File.open('app/assets/images/thining.jpg'), filename: 'thining.jpg')
+
+Workout.create!(id: "4",
+    user_id: "0",
+    menu: "デッドリフト",
+    content: "",
+    purpose: "1",
+    environment: "1",
+    target: "2",
+    video_id: "YXvcaAAToKo",
+)
+work = Workout.find(4)
+work.image.attach(io: File.open('app/assets/images/dead-lift.jpg'), filename: 'dead-lift.jpg')
+
 
 #マスタ
 Purpose.create!(id:1, name: '筋力トレーニング') 
